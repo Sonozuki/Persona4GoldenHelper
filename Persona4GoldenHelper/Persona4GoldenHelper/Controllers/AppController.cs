e9bc539dcc2b0cf2676cb16ba3fa98f4d88c6258
Andy Crocker <atmcrocker@gmail.com>
2019-12-19 16:43:09 +0000

    Merge ExamAnswerController and BookController into AppController

--- Persona4GoldenHelper/Persona4GoldenHelper/Controllers/AppController.cs
+++ Persona4GoldenHelper/Persona4GoldenHelper/Controllers/AppController.cs
@@ -1,12 +1,54 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Persona4GoldenHelper.Data;
+using Persona4GoldenHelper.Models;
 
 namespace Persona4GoldenHelper.Controllers
 {
     public class AppController : Controller
     {
+        private readonly IBook BookService;
+        private readonly IAnswer AnswerService;
+
+        public AppController(IBook bookService, IAnswer answerService)
+        {
+            BookService = bookService;
+            AnswerService = answerService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Books()
+        {
+            ViewBag.Title = "Books";
+
+            var model = new BookViewModel();
+            model.Books = BookService.GetAll();
+            model.BookOrder = BookService.GetOrder();
+
+            return View(model);
+        }
+
+        public IActionResult ExamAnswers()
+        {
+            ViewBag.Title = "Exam Answers";
+
+            var model = new ExamAnswerViewModel();
+            model.April = AnswerService.GetByMonth("April");
+            model.May = AnswerService.GetByMonth("May");
+            model.June = AnswerService.GetByMonth("June");
+            model.July = AnswerService.GetByMonth("July");
+            model.August = AnswerService.GetByMonth("August");
+            model.September = AnswerService.GetByMonth("September");
+            model.October = AnswerService.GetByMonth("October");
+            model.November = AnswerService.GetByMonth("November");
+            model.December = AnswerService.GetByMonth("December");
+            model.Janurary = AnswerService.GetByMonth("Janurary");
+            model.February = AnswerService.GetByMonth("February");
+
+            return View(model);
         }
     }
 }
