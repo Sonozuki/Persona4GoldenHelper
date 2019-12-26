5816b4b50eccf86cfd35c3e4b57a4687be443246
Andy Crocker <atmcrocker@gmail.com>
2019-12-26 19:43:13 +0000

    Add arcana ranks data

---
+++ Persona4GoldenHelper/Persona4GoldenHelper.Data/Data/Calculator/ArcanaRankData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persona4GoldenHelper.Data.Data.Calculator
+{
+    public class ArcanaRankData
+    {
+        public static List<string> Arcanas { get; set; } = new List<string>
+        {
+            "Fool",
+            "Magician",
+            "Priestess",
+            "Empress",
+            "Emperor",
+            "Hierophant",
+            "Lovers",
+            "Chariot",
+            "Justice",
+            "Hermit",
+            "Fortune",
+            "Strength",
+            "Hanged Man",
+            "Death",
+            "Temperance",
+            "Devil",
+            "Tower",
+            "Star",
+            "Moon",
+            "Sun",
+            "Judgment",
+            "Jester",
+            "Aeon",
+            "World"
+        };
+    }
+}
