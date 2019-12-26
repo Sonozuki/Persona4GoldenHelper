920cb98fdd395cb382a5974ce03405ac535537f5
Andy Crocker <atmcrocker@gmail.com>
2019-12-26 20:22:40 +0000

    Add special persona fusion results

---
+++ Persona4GoldenHelper/Persona4GoldenHelper.Data/Data/Calculator/SpecialFusionResultData.cs
@@ -0,0 +1,206 @@
+﻿using Persona4GoldenHelper.Data.Models.Calculator;
+using System.Collections.Generic;
+
+namespace Persona4GoldenHelper.Data.Data.Calculator
+{
+    public class SpecialFusionResultData
+    {
+        public List<SpecialFusionResult> SpecialFusions { get; set; } = new List<SpecialFusionResult>
+        {
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Nebiros",
+                    "Belial"
+                },
+                ResultPersona = "Alice"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Vetala",
+                    "Lilim"
+                },
+                ResultPersona = "Ukobach"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Rangda",
+                    "Barong"
+                },
+                ResultPersona = "Shiva"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Obariyon",
+                    "Yomotsu-Shikome"
+                },
+                ResultPersona = "Izanagi"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Jack Frost",
+                    "Orobas"
+                },
+                ResultPersona = "Pixie"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Shiva",
+                    "Parvati"
+                },
+                ResultPersona = "Ardha"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Eligor",
+                    "Nata Taishi"
+                },
+                ResultPersona = "Slime"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Clotho",
+                    "Lachesis",
+                    "Atropos"
+                },
+                ResultPersona = "Norn"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Saki Mitama",
+                    "Ara Mitama",
+                    "Kusi Mitama",
+                    "Nigi Mitama"
+                },
+                ResultPersona = "Neko Shogun"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Phoenix",
+                    "Gdon",
+                    "Yatagarasu",
+                    "Narasimha"
+                },
+                ResultPersona = "Tam Lin"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Oni",
+                    "Fuu-Ki",
+                    "Kin-Ki",
+                    "Sui-Ki"
+                },
+                ResultPersona = "Ongyo-Ki"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Genbu",
+                    "Seiryu",
+                    "Suzaku",
+                    "Byakko"
+                },
+                ResultPersona = "Kohryu"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Jack Frost",
+                    "Pyro Jack",
+                    "King Frost",
+                    "Pixie",
+                    "Ghoul"
+                },
+                ResultPersona = "Black Frost"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Ares",
+                    "Triglav",
+                    "Kin-Ki",
+                    "Atavaka",
+                    "Neko Shogun"
+                },
+                ResultPersona = "Futsunushi"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Makami",
+                    "Orthrus",
+                    "Mothman",
+                    "Thoth",
+                    "Narasimha"
+                },
+                ResultPersona = "Yatsufusa"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Masakado",
+                    "Shiki-Ouji",
+                    "Okuninushi",
+                    "Hachiman",
+                    "Hitokoto-Nushi"
+                },
+                ResultPersona = "Yoshitsune"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Matador",
+                    "White Rider",
+                    "Mother Harlot",
+                    "Daisoujou",
+                    "Hell Biker",
+                    "Trumpeter"
+                },
+                ResultPersona = "Mahakala"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Pazuzu",
+                    "Belphegor",
+                    "Belial",
+                    "Mot",
+                    "Seth",
+                    "Baal Zebul"
+                },
+                ResultPersona = "Beelzebub"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Matador",
+                    "White Rider",
+                    "Daisoujou",
+                    "Taotie",
+                    "Pabilsag",
+                    "Taowu"
+                },
+                ResultPersona = "Trumpeter"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Ananta",
+                    "Anubis",
+                    "Trumpeter",
+                    "Michael",
+                    "Satan",
+                    "Metatron"
+                },
+                ResultPersona = "Lucifer"
+            },
+            new SpecialFusionResult() {
+                SourcePersonas = new List<string>() {
+                    "Izanagi",
+                    "Sandman",
+                    "Nata Taishi",
+                    "Girimehkala",
+                    "Norn",
+                    "Okuninushi",
+                    "Orthrus",
+                    "Kartikeya",
+                    "Mithra",
+                    "Tzitzimitl",
+                    "Cu Chulainn",
+                    "Legion"
+                },
+                ResultPersona = "Izanagi-No-Okami"
+            }
+        };
+    }
+}
---
+++ Persona4GoldenHelper/Persona4GoldenHelper.Data/Models/Calculator/SpecialFusionResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Persona4GoldenHelper.Data.Models.Calculator
+{
+    public class SpecialFusionResult
+    {
+        public int Id { get; set; }
+        public List<string> SourcePersonas { get; set; }
+        public string ResultPersona { get; set; }
+    }
+}
