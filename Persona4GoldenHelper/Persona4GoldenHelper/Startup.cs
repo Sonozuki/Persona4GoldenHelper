3b90ce034db8e729252508e7bfce778da2bd159b
Andy Crocker <atmcrocker@gmail.com>
2020-01-02 21:56:11 +0000

    Allow anyone to all the api

--- Persona4GoldenHelper/Persona4GoldenHelper/Startup.cs
+++ Persona4GoldenHelper/Persona4GoldenHelper/Startup.cs
@@ -20,6 +20,14 @@ namespace Persona4GoldenHelper
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+                options.AddPolicy("AllowAll", policyBuilder =>
+                    policyBuilder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                    )
+                );
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
@@ -36,6 +44,8 @@ namespace Persona4GoldenHelper
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("AllowAll");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
