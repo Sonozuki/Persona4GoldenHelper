2ef1ae522c78174154dc3ec9437af4712b7920ae
Andy Crocker <atmcrocker@gmail.com>
2020-01-04 18:30:57 +0000

    Add return to top button on desktop

---
+++ Persona4GoldenHelper/Persona4GoldenHelper/wwwroot/scipts/site.js
@@ -0,0 +1,20 @@
+﻿$(window).on('scroll', () => {
+    // don't allow the return to top display on mobile (get's in the way)
+    if ($(window).width() < 768) {
+        $('#return-to-top').hide();
+        return;
+    }
+
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        $('#return-to-top').fadeIn(700);
+    }
+    else {
+        $('#return-to-top').fadeOut(700);
+    }
+});
+
+$('#return-to-top').on('click', () => {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+});
