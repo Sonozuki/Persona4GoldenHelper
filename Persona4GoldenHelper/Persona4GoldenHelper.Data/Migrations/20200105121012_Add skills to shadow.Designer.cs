9b9647870c79949fc4f79e5266b6d953ad05186a
Andy Crocker <atmcrocker@gmail.com>
2020-01-05 12:10:47 +0000

    Add skills on shadows to database

--- Persona4GoldenHelper/Persona4GoldenHelper.Data/Migrations/20200105121012_Add skills to shadow.Designer.cs
+++ Persona4GoldenHelper/Persona4GoldenHelper.Data/Migrations/20200105121012_Add skills to shadow.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persona4GoldenHelper.Data;
 
 namespace Persona4GoldenHelper.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200105121012_Add skills to shadow")]
+    partial class Addskillstoshadow
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persona4GoldenHelper/Persona4GoldenHelper.Data/Migrations/20200105121012_Add skills to shadow.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persona4GoldenHelper.Data.Migrations
+{
+    public partial class Addskillstoshadow : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ShadowId",
+                table: "Skills",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_ShadowId",
+                table: "Skills",
+                column: "ShadowId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Skills_Shadows_ShadowId",
+                table: "Skills",
+                column: "ShadowId",
+                principalTable: "Shadows",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Skills_Shadows_ShadowId",
+                table: "Skills");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Skills_ShadowId",
+                table: "Skills");
+
+            migrationBuilder.DropColumn(
+                name: "ShadowId",
+                table: "Skills");
+        }
+    }
+}
