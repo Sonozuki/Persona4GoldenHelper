f757713aab67bc50913c46d2084b469f6e7d3b5e
Andy Crocker <atmcrocker@gmail.com>
2021-07-24 16:33:54 +0100

    Add personas page

---
+++ Persona4GoldenHelper/Server/Controller/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Persona4GoldenHelper.Server.Data;
+using Persona4GoldenHelper.Shared.Models;
+using System.Collections.Generic;
+
+namespace Persona4GoldenHelper.Server.Controller
+{
+    /// <summary>Provides APIs that exposes all internal data.</summary>
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ApiController : ControllerBase
+    {
+        /*********
+        ** Public Methods
+        *********/
+        /// <summary>Retrieves all persona data.</summary>
+        /// <returns>All persona data.</returns>
+        [HttpGet]
+        public List<Persona> GetAllPersonas() => PersonaData.Personas;
+    }
+}
