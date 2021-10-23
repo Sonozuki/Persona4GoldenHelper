dcc1380ed9013a207f782be44512a9db25017eb2
Andy Crocker <atmcrocker@gmail.com>
2021-10-23 12:55:00 +0100

    Add individual persona pages

--- Persona4GoldenHelper/Utilities.cs
+++ Persona4GoldenHelper/Utilities.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace Persona4GoldenHelper
+﻿namespace Persona4GoldenHelper
 {
     /// <summary>Contains miscellaneous helper methods.</summary>
     public static class Utilities
@@ -27,5 +25,16 @@ namespace Persona4GoldenHelper
         /// <param name="element">The element affinity to convert</param>
         /// <returns>The short hand version of the affinity</returns>
         public static string ConvertElementAffinityToShortHand(ElementAffinity element) => AffinitityNameConversion[element.ToString()];
+
+        /// <summary>Calcualtes the CSS class for a specified cost.</summary>
+        /// <param name="cost">The cost to retrieve the CSS class for.</param>
+        /// <returns>The CSS class to use when styling the cost.</returns>
+        public static string CalculateCostClass(string cost)
+        {
+            if (cost.Length < 2)
+                return "";
+
+            return $"skill-cost-{cost[^2..].ToLower()}";
+        }
     }
 }
